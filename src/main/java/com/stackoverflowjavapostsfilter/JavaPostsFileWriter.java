57f8cfafc3cb655d4a2e50cd1b9e6fb1fe5be72b
RamaPramati <ramakrishna10410132@gmail.com>
2016-08-05 15:42:47 +0530

    Changed functionality: Used SAX instaed of DOM for xml parser

--- src/main/java/com/stackoverflowjavapostsfilter/JavaPostsFileWriter.java
+++ src/main/java/com/stackoverflowjavapostsfilter/JavaPostsFileWriter.java
@@ -1,14 +1,7 @@
 package com.stackoverflowjavapostsfilter;
 
-import javafx.util.Pair;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.w3c.dom.Element;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,50 +9,20 @@ import java.util.Map;
  * Created by ramakrishnas on 29/7/16.
  */
 class JavaPostsFileWriter {
+    static Map<String, Map<String,String>> javaPosts = new HashMap<String, Map<String,String>>();
 
-    static Map<String, String> javaPostsAnswers = new HashMap<String, String>();
-    static Map<String, Pair<String, Integer>> javaMostVotedAnswers = new HashMap<String, Pair<String, Integer>>();
+    static void writePostsWithRequiredAnswersToFile() {
 
-    static void writeToFile(Element postElement, int postTypeId) {
-
-        JSONObject javaPost = new JSONObject();
-        if (postTypeId == 1) {
-            javaPost.put("Post Id", postElement.getAttribute("Id"));
-            javaPost.put("Title", postElement.getAttribute("Title"));
-            javaPost.put("Description", postElement.getAttribute("Body"));
-            javaPost.put("AcceptedAnswerId", postElement.getAttribute("AcceptedAnswerId"));
-            StackOverFlowJavaPostsFilter.javaPostsQuestionsWriter.println(javaPost.toString() + ",");
-        } else if (postTypeId == 2) {
-            javaPostsAnswers.put(postElement.getAttribute("Id"), postElement.getAttribute("Body"));
-        } else {
-            if (javaMostVotedAnswers.containsKey(postElement.getAttribute("ParentId"))) {
-                Pair<String, Integer> tempAnswerScore = javaMostVotedAnswers.get(postElement.getAttribute("ParentId"));
-                if (tempAnswerScore.getValue() < Integer.valueOf(postElement.getAttribute("Score")))
-                    javaMostVotedAnswers.put(postElement.getAttribute("ParentId"),
-                            new Pair<String, Integer>(postElement.getAttribute("Body"), Integer.valueOf(postElement.getAttribute("Score"))));
-            } else
-                javaMostVotedAnswers.put(postElement.getAttribute("ParentId"),
-                        new Pair<String, Integer>(postElement.getAttribute("Body"), Integer.valueOf(postElement.getAttribute("Score"))));
-        }
-    }
-
-    static void writePostsWithRequiredAnswersToFile() throws IOException, ParseException {
-
-
-        JSONParser parser = new JSONParser();
-        JSONArray javaQuestionsJson = (JSONArray) parser.parse(new FileReader(StackOverFlowJavaPostsFilter.
-                applicationProperties.getProperty("filteredQuestionsFilePath")));
-
-        for (Object javaQuestionJson : javaQuestionsJson) {
-            JSONObject javaPost = new JSONObject();
-            JSONObject javaQuestion = (JSONObject) javaQuestionJson;
-            javaPost.put("Post Id", javaQuestion.get("Post Id"));
-            javaPost.put("Title", javaQuestion.get("Title"));
-            javaPost.put("Descrpition", javaQuestion.get("Description"));
-            javaPost.put("Accepted Answer", javaPostsAnswers.get(javaQuestion.get("AcceptedAnswerId")));
-            if (javaMostVotedAnswers.containsKey(javaQuestion.get("Post Id")))
-                javaPost.put("Most voted Answer", javaMostVotedAnswers.get(javaQuestion.get("Post Id")).getKey());
-            StackOverFlowJavaPostsFilter.javaPostsWriter.println(javaPost.toString() + "\n");
+        for (Map.Entry<String, Map<String,String>> javaPost: javaPosts.entrySet()) {
+            JSONObject javaPostWithRequiredAnswers = new JSONObject();
+            Map<String,String> postDetails = javaPost.getValue();
+            javaPostWithRequiredAnswers.put("Post Id", postDetails.get("Post Id"));
+            javaPostWithRequiredAnswers.put("Title", postDetails.get("Title"));
+            javaPostWithRequiredAnswers.put("Descrpition", postDetails.get("Description"));
+            javaPostWithRequiredAnswers.put("Accepted Answer", StackOverFlowPostsParser.javaPostsAnswers.get(postDetails.get("AcceptedAnswerId")));
+            if (StackOverFlowPostsParser.javaMostVotedAnswers.containsKey(postDetails.get("Post Id")))
+                javaPostWithRequiredAnswers.put("Most voted Answer", StackOverFlowPostsParser.javaMostVotedAnswers.get(postDetails.get("Post Id")).getKey());
+            StackOverFlowJavaPostsFilter.javaPostsWriter.println(javaPostWithRequiredAnswers.toString() + "\n");
         }
     }
 }
--- src/main/java/com/stackoverflowjavapostsfilter/StackOverFlowJavaPostsFilter.java
+++ src/main/java/com/stackoverflowjavapostsfilter/StackOverFlowJavaPostsFilter.java
@@ -1,18 +1,12 @@
 package com.stackoverflowjavapostsfilter;
 
-import org.json.simple.parser.ParseException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -22,61 +16,31 @@ import java.util.logging.Logger;
 public class StackOverFlowJavaPostsFilter {
 
     private static final Logger LOGGER = Logger.getLogger(StackOverFlowJavaPostsFilter.class.getName());
-
-    static Properties applicationProperties = new Properties();
-
+    private static Properties applicationProperties = new Properties();
     static PrintWriter javaPostsWriter;
-    static PrintWriter javaPostsQuestionsWriter;
 
     public static void main(String args[]) {
-        JavaPostsFilter javaPostsFilter = new JavaPostsFilter();
-
         try {
             applicationProperties.load(new FileInputStream("application.properties"));
+            javaPostsWriter = new PrintWriter(applicationProperties.getProperty("filteredPostsFilePath"));
 
             File stackOverFlowPostsFolder = new File(applicationProperties.getProperty("stackOverFlowPostsDirectory"));
-
-            javaPostsWriter = new PrintWriter(applicationProperties.getProperty("filteredPostsFilePath"));
-            javaPostsQuestionsWriter = new PrintWriter(applicationProperties.getProperty("filteredQuestionsFilePath"));
-
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             File[] postsFiles = stackOverFlowPostsFolder.listFiles();
-            List<String> acceptedAnswerIds = new ArrayList<String>();
-            List<String> javaPostsIds = new ArrayList<String>();
+
+            DefaultHandler handler = new StackOverFlowPostsParser();
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            factory.setValidating(false);
+            SAXParser parser = factory.newSAXParser();
 
             if (postsFiles == null) {
                 LOGGER.info("No files in the given directory");
                 return;
             }
-            javaPostsQuestionsWriter.println("[");
-
             for (File postsFile : postsFiles) {
                 LOGGER.info("Processing file " + postsFile);
-                Document postsDocument = dBuilder.parse(postsFile);
-                postsDocument.getDocumentElement().normalize();
-                NodeList postsDetails = postsDocument.getElementsByTagName("row");
-
-                for (int postDetailsIndex = 0; postDetailsIndex < postsDetails.getLength(); postDetailsIndex++) {
-                    Node post = postsDetails.item(postDetailsIndex);
-                    if (post.getNodeType() == Node.ELEMENT_NODE) {
-                        Element postElement = (Element) post;
-                        if ("1".equals(postElement.getAttribute("PostTypeId")) && (javaPostsFilter.isJavaPost(postElement))) {
-                            JavaPostsFileWriter.writeToFile(postElement, 1);
-                            acceptedAnswerIds.add(postElement.getAttribute("AcceptedAnswerId"));
-                            javaPostsIds.add(postElement.getAttribute("Id"));
-                        } else if (acceptedAnswerIds.contains(postElement.getAttribute("Id"))) {
-                            JavaPostsFileWriter.writeToFile(postElement, 2);
-                        } else if (javaPostsIds.contains(postElement.getAttribute("ParentId"))) {
-                            JavaPostsFileWriter.writeToFile(postElement, 3);
-                        }
-                    }
-                }
+                parser.parse(postsFile, handler);
             }
-            javaPostsQuestionsWriter.print("]");
-            javaPostsQuestionsWriter.close();
             JavaPostsFileWriter.writePostsWithRequiredAnswersToFile();
-
         } catch (SAXException saxException) {
             LOGGER.info("Failed to parse xml..." + saxException.getMessage());
         } catch (FileNotFoundException fileNotFoundException) {
@@ -85,11 +49,8 @@ public class StackOverFlowJavaPostsFilter {
             LOGGER.info("Failure in parser configuration...." + parserConfigurationException.getMessage());
         } catch (IOException ioException) {
             LOGGER.info("IOException" + ioException.getMessage());
-        } catch (ParseException parseException) {
-            LOGGER.info("ParseException" + parseException.getMessage());
         } finally {
             javaPostsWriter.close();
         }
     }
-
 }
